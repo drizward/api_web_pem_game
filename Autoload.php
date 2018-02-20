55aa341ec01c9302a843a145e51238fe1ce5f07e
drizward <rizwardh@gmail.com>
2018-02-20 22:09:58 +0700

    Autoload fixes

--- Autoload.php
+++ Autoload.php
@@ -1,12 +1,18 @@
 <?php
 
-const DEFAULT_NAMESPACE = 'Api';
+const DEFAULT_NAMESPACE = 'Biru';
+
+function str_replace_first($from, $to, $subject)
+{
+    $from = '/'.preg_quote($from, '/').'/';
+    return preg_replace($from, $to, $subject, 1);
+}
 
 spl_autoload_register(function($cls) {
     $cls = str_replace('\\', '/', $cls);
     $n = explode('/', $cls);
     if($n[0] == DEFAULT_NAMESPACE)
-        $cls = str_replace(DEFAULT_NAMESPACE, '', $cls);
+        $cls = str_replace_first(DEFAULT_NAMESPACE, '', $cls);
 
-    include __DIR__.$cls.'.php';
+    include __DIR__.'/'.$cls.'.php';
 });
